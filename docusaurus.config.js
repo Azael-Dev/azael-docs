5035ad737d1d895e736c36e8b6c189e4a2fedd58
Azael Dev <42119584+Azael-Dev@users.noreply.github.com>
2022-09-16 02:52:07 +0700

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,101 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/vsDark');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Azael Docs',
+  tagline: 'เอกสารประกอบการใช้งานสำหรับทรัพยากรที่ถูกพัฒนาโดย Azael Dev',
+  url: 'https://docs.azael.dev',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/logo.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Azael-Dev', // Usually your GitHub org/user name.
+  projectName: 'azael-docs', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'th',
+    locales: ['th'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          path: 'fivem',
+          routeBasePath: 'fivem',
+          showLastUpdateTime: true,
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/Azael-Dev/azael-docs/edit/main/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        defaultMode: 'dark'
+      },
+      navbar: {
+        title: 'Azael Docs',
+        logo: {
+          alt: 'Azael Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            href: 'https://www.azael.dev',
+            label: 'Azael.Dev',
+            position: 'right',
+          },
+          {
+            href: 'https://discord.gg/Ca5W62f',
+            label: 'Discord',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/Azael-Dev/azael-docs',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright ©${new Date().getFullYear()} Azael Dev, All Rights Reserved.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ['lua']
+      },
+    }),
+
+    themes: [
+      [
+        require.resolve('@easyops-cn/docusaurus-search-local'),
+        {
+          hashed: true,
+          //language: ['en', 'th'],
+        },
+      ],
+    ],
+};
+
+module.exports = config;
---
+++ fivem/index.md
@@ -0,0 +1,11 @@
+---
+title: FiveM
+sidebar_label: Introduction
+sidebar_position: 1
+---
+
+# FiveM Docs
+
+ยินดีต้อนรับสู่บ้านหลังใหม่ สำหรับเอกสารประกอบการใช้งานทรัพยากร **[FiveM](https://fivem.net/)** ที่ถูกพัฒนาโดย **[Azael Dev](https://www.azael.dev/)**
+
+สำหรับคำถามหรือปัญหาใดๆที่เกี่ยวข้องกับการใช้งาน อย่าลังเลที่จะติดต่อกับเราภายในชุมชน **[Azael Dev | Community](https://discord.gg/Ca5W62f)** บนแอปพลิเคชัน **[Discord](https://discord.com/)**
