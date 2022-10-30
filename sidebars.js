642a544abc5cc40a813804c2b9d3dff67a0c0a85
Azael Dev <42119584+Azael-Dev@users.noreply.github.com>
2022-10-31 00:11:58 +0700

    azael_dc-serverlogs (Example: gcphone)

--- sidebars.js
+++ sidebars.js
@@ -133,6 +133,7 @@ const sidebars = {
                     'script/azael_dc-serverlogs/example/other/esx_advancedgarage',
                     'script/azael_dc-serverlogs/example/other/esx_jb_eden_garage2',
                     'script/azael_dc-serverlogs/example/other/lls-mechanic',
+                    'script/azael_dc-serverlogs/example/other/gcphone',
                   ],
                 },
               ],
