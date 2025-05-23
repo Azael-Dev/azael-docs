008b907b72d3b33d20ea1ceffe445cc5eccc85b6
Azael Dev <42119584+Azael-Dev@users.noreply.github.com>
2025-05-23 22:19:49 +0700

    feat(commands): add command reference docs

--- sidebars.js
+++ sidebars.js
@@ -136,6 +136,7 @@ const sidebars = {
                 },
               ],
             },
+            'script/azael_playpass/commands',
           ],
         },
         /* azael_dc-serverlogs */
