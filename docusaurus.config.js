27094f11f9f90a3d8df4cb7bf8985cf0874367dd
Azael Dev <42119584+Azael-Dev@users.noreply.github.com>
2026-01-11 00:25:17 +0700

    chore(docusaurus.config): update Docusaurus baseUrl in config
    Changed the baseUrl in docusaurus.config.js from '/azael-docs/cfx/' to '/azael-docs/' to reflect the new documentation path.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   title: 'Azael Docs',
   tagline: 'เอกสารประกอบการใช้งานสำหรับทรัพยากรที่ถูกพัฒนาโดย Azael Dev',
   url: 'https://azael-dev.github.io',
-  baseUrl: '/azael-docs/cfx/',
+  baseUrl: '/azael-docs/',
   favicon: 'img/logo.ico',
   onBrokenLinks: 'throw',
   onBrokenAnchors: 'throw',
